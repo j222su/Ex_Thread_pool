89bfcb3df6464d1eda515b864ec885437f75875f
j222su <patrickstar0910@gmail.com>
2021-10-21 18:56:23 +0900

    ADD : Timer

--- src/com/js/example/Main.java
+++ src/com/js/example/Main.java
@@ -1,23 +1,27 @@
 package com.js.example;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
 
+    static int count = 1;
+
     public static void main(String[] args) {
         // 1. newCachedThreadPool
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        for(int i=0; i<=10; i++) {
-            MyRunnable myRunnable = new MyRunnable();
-            executorService.submit(myRunnable);
-//            executorService.execute(myRunnable);
+        MyRunnable myRunnable[] = new MyRunnable[2];
+
+        for(int i=0; i<2; i++) {
+            myRunnable[i] = new MyRunnable(i+1);
+            executorService.submit(myRunnable[i]);
         }
 
         //작업큐에 남아있는 작업이 모두 완료되었을 때 종료시킨다.
-        executorService.shutdown();
-
-
+        //executorService.shutdown();
     }
 }
--- src/com/js/example/MyRunnable.java
+++ src/com/js/example/MyRunnable.java
@@ -1,9 +1,31 @@
 package com.js.example;
 
-public class MyRunnable implements Runnable{
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyRunnable implements Runnable {
+
+    int index = 0;
+    int count = 1;
+
+    public MyRunnable(int index) {
+        this.index = index;
+    }
+
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
-        int value = Integer.parseInt("예외");
+        System.out.println(index+"번 스레드 생성");
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(index+"카운트 : "+count);
+                count++;
+            }
+        };
+        timer.schedule(timerTask, 1000, 1000);
+
+
     }
 }
